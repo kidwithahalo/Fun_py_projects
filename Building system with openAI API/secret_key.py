cdcc91061a9c0aff823f1bd484d7b4e52fa703e4
Pankaj K <pankajks@iitk.ac.in>
2023-08-12 04:09:08 +0530

    sync

---
+++ Building system with openAI API/secret_key.py
@@ -0,0 +1,7 @@
+OPENAI_API_KEY_1 = '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'
+
+# OPENAI_API_KEY_1 = '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'
+# this was from pankajkxx7 account and this had expired
+
+
+OPENAI_API_KEY = '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'
